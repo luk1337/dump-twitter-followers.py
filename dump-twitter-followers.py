387141de1cb2596cf8aa7e8c656b9fb0d0796f38
LuK1337 <priv.luk@gmail.com>
2020-03-16 13:05:42 +0100

    Move request headers to config.py

--- dump-twitter-followers.py
+++ dump-twitter-followers.py
@@ -3,17 +3,12 @@ import json
 import requests
 import sys
 
+from config import *
+
 
 def get_followers(user_id: int, cursor: int = -1):
-    # extract these using your favorite browser's dev tools.
-    headers = {
-        'x-csrf-token': '',
-        'authorization': '',
-        'cookie': '',
-    }
-
     url = f'https://api.twitter.com/1.1/friends/list.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cursor={cursor}&user_id={user_id}&count=20'
-    data = json.loads(requests.get(url, headers=headers).content)
+    data = json.loads(requests.get(url, headers=REQUEST_HEADERS).content)
 
     for user in data['users']:
         print(f'https://twitter.com/{user["screen_name"]}')
